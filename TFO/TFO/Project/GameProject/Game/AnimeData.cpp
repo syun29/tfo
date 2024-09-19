4361f336d062159f1a636bdb08f0e19799f00e65
矢 <24051910@d.anabuki-college.net>
2024-09-19 14:15:10 +0900

    j

--- TFO/TFO/Project/GameProject/Game/AnimeData.cpp
+++ TFO/TFO/Project/GameProject/Game/AnimeData.cpp
@@ -16,7 +16,22 @@ static TexAnim playerIdle[] = {
 	{ 11,2 }
 	
 };
+static TexAnim playerRun[] = {
+	{ 12,2 },
+	{ 13,2 },
+	{ 14,2 },
+	{ 15,2 },
+	{ 16,2 },
+	{ 17,2 },
+	{ 18,2 },
+	{ 19,2 },
+	{ 20,2 },
+	{ 21,2 },
+	{ 22,2 },
+	{ 23,2 },
+};
 TexAnimData player_anim_data[] = {
 	ANIMDATA(playerIdle),
+	ANIMDATA(playerRun),
 	
 };
