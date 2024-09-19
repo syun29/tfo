922dec4d6fd9f5c9f3caa7ba2f859e590903fcaf
kazumu <24051907@d.anabuki-college.net>
2024-09-19 12:33:10 +0900

    a

--- TFO/TFO/Project/GameProject/Game/Field.h
+++ TFO/TFO/Project/GameProject/Game/Field.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "../Base/Base.h"
+
+class Field : public Base {
+private:
+
+	CImage m_foreground;
+	CImage m_town;
+	CImage m_sky;
+	static float m_ground_y;
+public:
+	Field();
+	void Draw();
+	static float GetGroundY() {
+		return m_ground_y;
+	}
+};
