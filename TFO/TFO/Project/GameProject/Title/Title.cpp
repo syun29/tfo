bd37bbdfcebe0a9f8fffdfea68d4bc817cb30014
矢 <24051910@d.anabuki-college.net>
2024-09-30 10:23:28 +0900

    ji

---
+++ TFO/TFO/Project/GameProject/Title/Title.cpp
@@ -0,0 +1,28 @@
+#include "Title.h"
+#include "../Game/Game.h"
+Title::Title() :Base(eType_Scene),
+m_title_text("C:\\Windows\\Fonts\\msgothic.ttc", 64)
+{
+	m_img = COPY_RESOURCE("Title", CImage);
+	m_cnt = 0;
+}
+
+void Title::Update()
+{
+	//ボタン１でタイトル破棄
+	if (m_cnt++ > 60 && PUSH(CInput::eButton1)) {
+		//全てのオブジェクトを破棄
+		Base::KillAll();
+		//ゲームシーンへ
+		Base::Add(new Game());
+	}
+}
+
+void Title::Draw()
+{
+	m_img.Draw();
+	//文字表示
+	m_title_text.Draw(64, 256, 0, 0, 0, "Title");
+	m_title_text.Draw(64, 512, 0, 0, 0, "Push Z");
+}
+
