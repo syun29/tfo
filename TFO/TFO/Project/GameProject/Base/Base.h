e42cccecb8c13befe8f048e8260d0bb46164835c
山本翔太 <24051911@d.anabuki-college.net>
2024-10-03 11:52:08 +0900

    a

--- TFO/TFO/Project/GameProject/Base/Base.h
+++ TFO/TFO/Project/GameProject/Base/Base.h
@@ -17,6 +17,7 @@ enum {
     eType_Scene,
     eType_Map,
     eType_gimmick,
+    eType_Button,
 };
 //d—Í‰Á‘¬“x
 #define GRAVITY (9.8f/20)
---
+++ TFO/TFO/Project/GameProject/Game/Button.cpp
@@ -0,0 +1,47 @@
+#include "Button.h"
+#include "AnimeData.h"
+
+Button::Button(BaseType type, const CVector2D& p) : Base(type)
+{
+	//画像複製
+	m_img = COPY_RESOURCE("Button", CImage);
+	//画像サイズ設定
+	m_img.SetSize(64, 64);
+	//中心位置設定
+	m_img.SetCenter(0, 0);
+	m_rect = CRect(0, 0, 64, 64);
+	//再生アニメーション設定
+	m_img.ChangeAnimation(0);
+	//座標設定
+	m_pos = p;
+
+	m_switch = false;
+}
+
+void Button::Update()
+{
+	if (m_switch == false)
+		m_img.ChangeAnimation(eAnimIdle);
+	if (m_switch == true)
+		m_img.ChangeAnimation(eAnimWalk);
+	//アニメーション更新
+	m_img.UpdateAnimation();
+}
+
+void Button::Draw()
+{
+	Base::DrawImage();
+}
+
+void Button::Collision(Base* b)
+{
+	switch (b->m_type)
+	{
+	case eType_Player:
+		if (CollisionRect(this, b))
+		{
+			m_switch = true;
+		}
+		break;
+	}
+}
