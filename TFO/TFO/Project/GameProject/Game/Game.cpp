29534855dd3e8a1251e2f3c1f33ea5edd370b10c
kazumu <24051907@d.anabuki-college.net>
2024-10-04 12:05:42 +0900

    a

--- TFO/TFO/Project/GameProject/Game/Game.cpp
+++ TFO/TFO/Project/GameProject/Game/Game.cpp
@@ -8,9 +8,9 @@ Game::Game() :Base(eType_Scene)
 {
 	
 	SOUND("SE_Wall")->Load("Sound/kirikae.wav", 10);
-	Base::Add(new Player(CVector2D(650, 100), false, true));
+	Base::Add(new Player(CVector2D(150, 90), false, true));
 
-	Base::Add(new Player(CVector2D(500, 100), false, false));
+	Base::Add(new Player(CVector2D(200, 850), false, false));
 	Base::Add(new Map(1));
 }
 
