4c52ce62d9f0b70174cf4c99c93e17c1cd513d17
矢 <24051910@d.anabuki-college.net>
2024-10-04 12:09:57 +0900

    l

--- TFO/TFO/Project/GameProject/Game/Game.cpp
+++ TFO/TFO/Project/GameProject/Game/Game.cpp
@@ -12,7 +12,7 @@ Game::Game() :Base(eType_Scene)
 	Base::Add(new Player(CVector2D(650, 100), false, true));
 
 	Base::Add(new Player(CVector2D(500, 100), false, false));
-	Base::Add(new Button(eType_Button1, CVector2D(10 * MAP_TIP_SIZE, 0 * MAP_TIP_SIZE)));
+	Base::Add(new Button(eType_Button1, CVector2D(10 * MAP_TIP_SIZE, 9 * MAP_TIP_SIZE)));
 	Base::Add(new Map(1));
 }
 
