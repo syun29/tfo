ced63e6387f5a2af96ff5e261892709eb8288812
山本翔太 <24051911@d.anabuki-college.net>
2024-10-07 11:00:42 +0900

    a

--- TFO/TFO/Project/GameProject/Game/gimmick.cpp
+++ TFO/TFO/Project/GameProject/Game/gimmick.cpp
@@ -1,7 +1,22 @@
 #include"gimmick.h"
 
+gimmick::gimmick(const CVector2D& p, bool flip) :
+	Base(eType_gimmick) {
+	//画像複製
+	m_img = COPY_RESOURCE("dossun", CImage);
+	//再生アニメーション設定
+	m_img.ChangeAnimation(0);
+	//座標設定
+	m_pos = p;
+	//中心位置設定
+	m_img.SetCenter(128, 224);
+	//反転フラグ
+	m_flip = flip;
+	
+
 void gimmick::StateIdle()
 {
+	
 }
 
 void gimmick::StateDamage()
