7b25cbf42367a933ef923cd7ebd877ee8215b471
矢 <24051910@d.anabuki-college.net>
2024-10-09 14:48:53 +0900

    da

--- TFO/TFO/Project/GameProject/Game/Player.cpp
+++ TFO/TFO/Project/GameProject/Game/Player.cpp
@@ -36,8 +36,7 @@ void Player::StateIdle()
 	if (m_change && m_is_ground && PUSH(CInput::eButton2)) {
 		m_vec.y = -jump_pow;
 		
-
-		m_is_ground = true;
+		m_is_ground = false;
 	}
 	//ジャンプ中なら
 	if (!m_is_ground) {
@@ -149,26 +148,29 @@ void Player::Collision(Base* b)
 		};
 		break;
 	case eType_Areachange:
-		
-		if (PUSH(CInput::eUp)) {
+		if ( PUSH(CInput::eUp)) {
 			Button* bt = dynamic_cast<Button*>(Base::FindObject(eType_Button1));
 			if (bt->m_switch == true) {
 				if (Areachange* s = dynamic_cast<Areachange*>(b)) {
 					if (Base::CollisionRect(this, s)) {
 						Base::Kill(
 							1 << eType_Areachange
-							
-							| 1 << eType_Button1
+							| 1<< eType_Button1
 							| 1 << eType_Player
 							| 1 << eType_Door
 							| 1 << eType_Map
 							| 1 << eType_Goal);
-		
 
-						
+						m_pos_old = m_pos = s->GetNextPos();
+						Base::Add(new Map(s->GetNextArea()));
+						Base::Add(new Player(CVector2D(200, 850), false, true));
+						Base::Add(new Player(CVector2D(150, 850), false, false));
+					    Base::Add(new Goal(CVector2D(150, 850)));
 					}
 				}
-					break;
+			}
+		}
+		break;
 	case eType_Map:
 		if (Map* m = dynamic_cast<Map*>(b)) {
 			int t = m->CollisionRect(CVector2D(m_pos.x, m_pos_old.y), m_rect);
@@ -199,11 +201,23 @@ void Player::Collision(Base* b)
 				//設置フラグON
 				m_is_ground = true;
 			}
+			if (m_pos.y >1000) {
+				SetKill();
+			}
 		}
 		break;
 
-
-
-				}
+		
+			//ゴール判定
+		case eType_Goal:
+			if (Base::CollisionRect(this, b)) {
+				b->SetKill();
 			}
+			break;
+
+		
+	}
+}
+
+
 
