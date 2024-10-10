bf478387453de203f415d7e837745dccfb111fbd
山本翔太 <24051911@d.anabuki-college.net>
2024-10-10 11:31:51 +0900

    a

--- TFO/TFO/Project/GameProject/Game/Game.cpp
+++ TFO/TFO/Project/GameProject/Game/Game.cpp
@@ -24,7 +24,7 @@ Game::Game() :Base(eType_Scene)
 	Base::Add((new Button(eType_Button1, CVector2D(1650,266))));
 
 	Base::Add((new gimmick(eType_gimmick1, CVector2D(150,600))));
-}
+}aaa
 
 Game::~Game()
 {
